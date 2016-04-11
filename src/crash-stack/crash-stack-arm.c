a3c6ba5287848c31992739fadbc21a42a58f7323
Adrian Szyndela <adrian.s@samsung.com>
2016-04-11 12:32:31 +0200

    support for libelf 0.153 AND later up to 0.165
    Change-Id: If3927cbb1438115c7de2397a11ea91be9b9329fe

--- src/crash-stack/crash-stack-arm.c
+++ src/crash-stack/crash-stack-arm.c
@@ -5,6 +5,7 @@
 
 static Elf *g_core = NULL;
 static Dwfl *g_dwfl = NULL;
+static Mappings *g_mappings = NULL;
 
 struct Regs
 {
@@ -58,7 +59,7 @@ static Boolean report (void *data, Int32 address)
 Boolean readT (Int32 a, void *v, size_t size)
 {
   Dwfl_Module *module = 0;
-  Boolean result = FALSE;
+  Elf_Data *data = NULL;
 
   int segment = dwfl_addrsegment (g_dwfl, a, &module);
 
@@ -70,30 +71,51 @@ Boolean readT (Int32 a, void *v, size_t size)
     GElf_Addr bias;
     Elf *elf = dwfl_module_getelf (module, &bias);
 
-    Elf_Data *data = elf_getdata_rawchunk (elf, a-start, size, ELF_T_BYTE);
-    if (data != NULL)
-    {
-      memcpy (v, data->d_buf, size);
-      result = TRUE;
-    }
+    data = elf_getdata_rawchunk (elf, a-start, size, ELF_T_BYTE);
   }
-  if (!result && segment != -1)
+  if (NULL == data && segment != -1)
   {
     // get data from segment
     GElf_Phdr mem;
     GElf_Phdr *phdr = gelf_getphdr (g_core, segment, &mem);
     Dwarf_Addr offset_in_segment = a - phdr->p_vaddr;
-    Dwarf_Addr offset_in_file = phdr->p_offset + offset_in_segment;
-
-    Elf_Data *data = elf_getdata_rawchunk (g_core, offset_in_file, size, ELF_T_BYTE);
-    if (data != NULL)
+    if (offset_in_segment < phdr->p_filesz)
     {
-      memcpy (v, data->d_buf, size);
-      result = TRUE;
+      Dwarf_Addr offset_in_file = phdr->p_offset + offset_in_segment;
+
+      data = elf_getdata_rawchunk (g_core, offset_in_file, size, ELF_T_BYTE);
     }
   }
 
-  return result;
+  if (NULL == data && module != NULL)
+  {
+    const char *name = dwfl_module_info (module, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    if (name != NULL && name[0] == '[')
+    {
+      int i;
+      // get module from mappings
+      for (i = 0; i < g_mappings->elems; i++)
+      {
+        if (g_mappings->tab[i].m_start <= a && a < g_mappings->tab[i].m_end)
+        {
+          // compute offset relative to the start of the mapping
+          Int32 offset = a - g_mappings->tab[i].m_start;
+          // read from the file, but also account file offset
+          data = elf_getdata_rawchunk (g_mappings->tab[i].m_elf,
+              offset + g_mappings->tab[i].m_offset, size, ELF_T_BYTE);
+          break;
+        }
+      }
+    }
+  }
+
+  if (data != NULL)
+  {
+    memcpy (v, data->d_buf, size);
+    return TRUE;
+  }
+
+  return FALSE;
 }
 
 static Boolean readW (Int32 a, Int32 *v)
@@ -117,15 +139,59 @@ static Int32 getProloguePC (Int32 current_pc)
     Dwfl_Module *module = dwfl_addrmodule (g_dwfl, current_pc);
     if (module)
     {
-        GElf_Off offset;
+//        GElf_Off offset;
         GElf_Sym sym;
-        dwfl_module_addrinfo (module, current_pc, &offset, &sym, NULL, NULL, NULL);
-        result = current_pc - offset;
+//        dwfl_module_addrinfo (module, current_pc, &offset, &sym, NULL, NULL, NULL);
+        dwfl_module_addrsym (module, current_pc, &sym, NULL);
+//        result = current_pc - offset;
+        result = sym.st_value;
+    }
+    if (0 == result)
+    {
+      int i;
+      for (i=0; i < g_mappings->elems; i++)
+      {
+        if (g_mappings->tab[i].m_start <= current_pc && current_pc < g_mappings->tab[i].m_end)
+        {
+          /* go through symbols to find the nearest */
+          Elf_Scn *scn = NULL;
+          Elf *elf = g_mappings->tab[i].m_elf;
+          while ((scn = elf_nextscn (elf, scn)) != NULL)
+          {
+            GElf_Shdr shdr_mem;
+            GElf_Shdr *shdr = gelf_getshdr (scn, &shdr_mem);
+            if (shdr != NULL && (shdr->sh_type == SHT_SYMTAB || shdr->sh_type == SHT_DYNSYM))
+            {
+              Elf_Data *sdata = elf_getdata (scn, NULL);
+              unsigned int nsyms = sdata->d_size / (gelf_getclass(elf) == ELFCLASS32 ?
+                  sizeof (Elf32_Sym) :
+                  sizeof (Elf64_Sym));
+              unsigned int cnt;
+              uintptr_t address_offset = current_pc;
+              if (shdr->sh_type == SHT_DYNSYM)
+                address_offset -= g_mappings->tab[i].m_start;
+              for (cnt = 0; cnt < nsyms; ++cnt)
+              {
+                GElf_Sym sym_mem;
+                Elf32_Word xndx;
+                GElf_Sym *sym = gelf_getsymshndx(sdata, NULL, cnt, &sym_mem, &xndx);
+                if (sym != NULL && sym->st_shndx != SHN_UNDEF)
+                {
+                  if (sym->st_value <= address_offset && address_offset < sym->st_value + sym->st_size)
+                  {
+                    return sym->st_value;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }
     return result;
 }
 
-void create_crash_stack (Regs *regs, Dwfl *dwfl, Elf *core, Callstack *callstack)
+void create_crash_stack (Regs *regs, Dwfl *dwfl, Elf *core, Mappings *mappings, Callstack *callstack)
 {
   UnwindCallbacks callbacks =
   {
@@ -143,6 +209,7 @@ void create_crash_stack (Regs *regs, Dwfl *dwfl, Elf *core, Callstack *callstack
 
   g_dwfl = dwfl;
   g_core = core;
+  g_mappings = mappings;
 
   callstack->tab[0] = regs->pc;
   callstack->elems = 1;
